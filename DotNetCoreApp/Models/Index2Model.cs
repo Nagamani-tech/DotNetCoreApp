59d84f90650f48f1d2acd4188e6fbcacd8fb6901
Vadde Nagamani <vadde.nagamani@ltts.com>
2021-05-17 18:36:22 +0530

    commit

---
+++ DotNetCoreApp/Models/Index2Model.cs
@@ -0,0 +1,24 @@
+﻿using DotNetCoreApp.Interfaces;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreApp.Models
+{
+    public class Index2Model : PageModel
+    {
+        private readonly IMyDependency _myDependency;
+
+        public Index2Model(IMyDependency myDependency)
+        {
+            _myDependency = myDependency;
+        }
+
+        public void OnGet()
+        {
+            _myDependency.WriteMessage("Index2Model.OnGet");
+        }
+    }
+}
---
+++ DotNetCoreApp/Services/MyDependency.cs
@@ -0,0 +1,16 @@
+﻿using DotNetCoreApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreApp.Services
+{
+    public class MyDependency :IMyDependency
+    {
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine($"MyDependency.WriteMessage Message: {message}");
+        }
+    }
+}
